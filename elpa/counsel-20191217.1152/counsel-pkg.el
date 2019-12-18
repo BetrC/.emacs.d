bb2dc1e720067b8acd8e8cb3b8078e7d2cc3e371
BetrC <xj19971@gmail/com>
2019-12-19 02:26:20 +0800

    first commit

---
+++ elpa/counsel-20191217.1152/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20191217.1152" "Various completion functions using Ivy" '((emacs "24.5") (swiper "0.13.0")) :commit "41bd9886a5206d24f31cc3c4e33f5c5d72bff408" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
